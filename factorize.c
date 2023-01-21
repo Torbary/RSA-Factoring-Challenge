c5a4450a45080148fa942b63dd59e6cd823a7f2a
Torbary <torbarey020@gmail.com>
2023-01-21 05:47:46 -0800

    betty and pycodestyle modified

--- factorize.c
+++ factorize.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 /**
- * The function works by starting at 2, testing if the input number is divisible by 2. 
- * If it is divisible, the number is composite and we return 0. 
- * If it's not, we test every odd number greater than 2 as a divisor of the input number. 
+ * division - checks wether the number is divisible by 2
+ * @n: number
+ * Return: 0
+ * The function works by starting at 2,
+ * testing if the input number is divisible by 2
+ * If it is divisible, the number is composite and we return 0.
+ * If it's not, we test every odd number greater than 2 as a divisor of
+ * the input number.
  * If we find a divisor, the number is composite, otherwise, it's prime.
  */
 
@@ -11,18 +16,18 @@ int division(long int n)
 {
 	long int r;
 
-	if (n%2 == 0)
+	if (n % 2 == 0)
 	{
-		printf("%lu=%lu*%i\n", n, n/2, 2);
+		printf("%lu=%lu*%i\n", n, n / 2, 2);
 		return (0);
 	}
 
 	r = 3;
-	while (r*r <= n )
+	while (r * r <= n)
 	{
-		if (n%r == 0)
+		if (n % r == 0)
 		{
-			printf("%lu=%lu*%lu\n", n, n/r, r);
+			printf("%lu=%lu*%lu\n", n, n / r, r);
 			return (0);
 		}
 		else
